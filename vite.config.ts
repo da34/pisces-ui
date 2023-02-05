7fa0656c2a7b237981505b520161226bab672212
da34 <1255029618@qq.com>
2023-02-05 13:47:43 +0800

    feat: add test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Unocss from './config/unocss'
@@ -31,5 +32,16 @@ export default defineConfig({
         exports: 'named'
       },
     },
+  },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
   }
 });
