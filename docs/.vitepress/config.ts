62d6c0f1c4261c9a562da7c32f87bbc66b27abca
da34 <1255029618@qq.com>
2023-02-05 13:23:10 +0800

    feat: add docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,27 @@
+import { demoBlockPlugin } from 'vitepress-theme-demoblock'
+export default {
+  themeConfig: {
+    sidebar: {
+      '/': [
+        { text: '快速开始', link: '/' },
+        {
+          text: '通用',
+          items: [
+            { text: 'Button 按钮', link: '/components/button/' },
+          ]
+        },
+        { text: '导航' },
+        { text: '反馈' },
+        { text: '数据录入' },
+        { text: '数据展示' },
+        { text: '布局' },
+      ]
+    }
+  },
+  markdown: {
+    config: (md) => {
+      // 添加DemoBlock插槽
+      md.use(demoBlockPlugin)
+    }
+  }
+}
---
+++ docs/components/button/index.md
@@ -0,0 +1,7 @@
+  <div style="margin-bottom:20px;">
+    <YButton color="blue">主要按钮</YButton>
+    <YButton color="green">绿色按钮</YButton>
+    <YButton color="gray">灰色按钮</YButton>
+    <YButton color="yellow">黄色按钮</YButton>
+    <YButton color="red">红色按钮</YButton>
+  </div>
